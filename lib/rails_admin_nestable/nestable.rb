aff1b5e74efa204e75efa0db0e8409d0e7d53efe
Davide Cenzi <link82@gmail.com>
2020-08-27 10:51:58 +0200

    fix: added condition to avoid filtering record if model does not implement nestable_filters scope

--- lib/rails_admin_nestable/nestable.rb
+++ lib/rails_admin_nestable/nestable.rb
@@ -77,8 +77,10 @@ module RailsAdmin
               
               query = list_entries(@model_config, :nestable, false, false).reorder(nil)
               
-              @parent_id = params[:parent_id]
-              query = query.nestable_children_of(@parent_id) 
+              if @abstract_model.model.respond_to?(:nestable_filters)
+                @parent_id = params[:parent_id]
+                query = query.nestable_children_of(@parent_id)
+              end
               
 
               case @options[:scope].class.to_s
